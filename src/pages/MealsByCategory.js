183995d53932ce44af46e0df11a03a28ee8650fd
Starfloat <lakhaisys@gmail.com>
2022-01-08 00:07:04 -0500

    pagination added

--- src/pages/MealsByCategory.js
+++ src/pages/MealsByCategory.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import MealItemLessList from "../components/MealGenerator/MealItemLessList";
 
+import Pagination from "../components/ui/Pagination";
+
 const MealsByCategory = () => {
   const { category } = useParams();
   const [meals, setMeals] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [mealsPerPage] = useState(2);
 
   useEffect(() => {
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
@@ -12,7 +16,24 @@ const MealsByCategory = () => {
       .then((data) => setMeals(data.meals));
   }, [category]);
 
-  return <MealItemLessList meals={meals} />;
+  // Get current meals
+  const indexOfLastMeal = currentPage * mealsPerPage;
+  const indexofFirstMeal = indexOfLastMeal - mealsPerPage;
+  const currentMeals = meals.slice(indexofFirstMeal, indexOfLastMeal);
+
+  //change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <>
+      <MealItemLessList meals={currentMeals} />
+      <Pagination
+        mealsPerPage={mealsPerPage}
+        totalMeals={meals.length}
+        paginate={paginate}
+      />
+    </>
+  );
 };
 
 export default MealsByCategory;
